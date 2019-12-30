801deefdb07b9c1051e65680b477b31cc687f480
Timo Yan <tomyan13@gmail.com>
2019-12-30 23:57:29 +0800

    Add.

--- packages/sample/packages/client/pages/home/index.tsx
+++ packages/sample/packages/client/pages/home/index.tsx
@@ -1,9 +1,12 @@
-import { utcToZonedTime } from 'date-fns-tz';
+import { format, utcToZonedTime } from 'date-fns-tz';
 import React from 'react';
 
 const timeZone = 'America/Los_Angeles';
 const zonedDate = utcToZonedTime(new Date().toISOString(), timeZone);
 
+const pattern = 'd.MMM.yyyy HH:mm:ss OOOO';
+const output = format(zonedDate, pattern, { timeZone }).toString();
+
 export function Home(): JSX.Element {
-    return <div>{zonedDate.toJSON()}</div>;
+    return <div>{output}</div>;
 }
